52f9ea44050d95a24a2700c4c5ffe7c69823d148
Elliot Parrish <elliotparrish@gmail.com>
2022-02-16 15:15:05 -0500

    Initial commit.

---
+++ Territory_Profit_Plotting.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+	"""
+	import data from csv file, plot multi line graph, and save it.
+	"""
+
+	df = pd.read_csv("Yaxche_Territories.csv")
+
+	print(df.head())
+
+	small_df = df[["Territory","3","4","2","1"]]
+	small_df["4"]=small_df["4"].astype(float)
+	small_df["3"]=small_df["3"].astype(float)
+	small_df["2"]=small_df["2"].astype(float)
+	small_df["1"]=small_df["1"].astype(float)
+	print(small_df.head())
+	small_df=small_df.pivot_table(columns="Territory")
+	small_df = small_df[["Restless Shores", "Monarch's Bluffs", "Reekwater", "Weaver's Fen", "Cutlass Keys", "Mourningdale", "First Light"]] #"Brightwood", "Everfall", "Windsward", "Ebonscale Reach"]]
+	print(small_df.head())
+	ax = small_df.plot(ylim=(-30000,200000), lw=2, marker='.', markersize=10, title='Profits by Territory', grid=True)
+	# ax.set(xlabel="Week", ylabel="Profit in Coin")
+	ax.set_ylabel("Profit in Coin", labelpad=0)
+	ax.set_xlabel("Week")
+	plt.legend(ncol=3)
+	ax.get_figure().savefig("Lower_Profits.png")
+	return 0
+
+if __name__ == "__main__":
+	main()
