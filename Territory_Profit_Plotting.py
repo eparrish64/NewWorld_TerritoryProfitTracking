eae4a8b8df1c4f1ad4c04615ec1cded796f836e1
Elliot Parrish <elliotparrish@gmail.com>
2022-04-11 13:19:21 -0400

    Changed from Yaxche to Ohonoo naming.

--- Territory_Profit_Plotting.py
+++ Territory_Profit_Plotting.py
@@ -6,7 +6,7 @@ def main():
 	import data from csv file, plot multi line graph, and save it.
 	"""
 
-	df = pd.read_csv("Yaxche_Territories.csv")
+	df = pd.read_csv("Ohono_Territories.csv")
 
 	print(df.head())
 
@@ -17,14 +17,26 @@ def main():
 	small_df["1"]=small_df["1"].astype(float)
 	print(small_df.head())
 	small_df=small_df.pivot_table(columns="Territory")
-	small_df = small_df[["Restless Shores", "Monarch's Bluffs", "Reekwater", "Weaver's Fen", "Cutlass Keys", "Mourningdale", "First Light"]] #"Brightwood", "Everfall", "Windsward", "Ebonscale Reach"]]
+	small_df = small_df[[
+	# "Restless Shores", 
+	"Monarch's Bluffs",
+	 # "Reekwater", 
+	 "Weaver's Fen", 
+	 "Cutlass Keys", 
+	 "Mourningdale", 
+	 "First Light", 
+	 # "Brightwood", 
+	 "Everfall", 
+	 # "Windsward", 
+	 "Ebonscale Reach",
+	 ]]
 	print(small_df.head())
-	ax = small_df.plot(ylim=(-30000,200000), lw=2, marker='.', markersize=10, title='Profits by Territory', grid=True)
+	ax = small_df.plot(ylim=(0,2100000), lw=2, marker='.', markersize=10, title='Profits by Territory', grid=True)
 	# ax.set(xlabel="Week", ylabel="Profit in Coin")
 	ax.set_ylabel("Profit in Coin", labelpad=0)
 	ax.set_xlabel("Week")
-	plt.legend(ncol=3)
-	ax.get_figure().savefig("Lower_Profits.png")
+	plt.legend(ncol=2)
+	ax.get_figure().savefig("Profits_Non_Yellow.png")
 	return 0
 
 if __name__ == "__main__":
